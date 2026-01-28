b0094e7a49ca6418a9627cc2e0b6ba247bbc01c9
Ignacio Fernández Suárez <ignaciofsoviedo@gmail.com>
2026-01-28 09:29:24 +0100

    Add NackEngine project with initial Vector class

---
+++ NackEngine/Vector.cs
@@ -0,0 +1,19 @@
+﻿namespace NackEngine
+{
+    public class Vector
+    {
+        private double[] v = new double[3];
+        public Vector() {
+            this.v = [0, 0, 0];
+        }
+
+        public Vector(double v1, double v2, double v3) {
+            this.v[0] = v1;
+            this.v[1] = v2;
+            this.v[2] = v3;
+        }
+
+        public double X() { return this.v[0];}
+            
+    }
+}
