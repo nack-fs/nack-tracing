fe9eac21d486052fd5c827de6e58888c01f309dc
Ignacio Fernández Suárez <ignaciofsoviedo@gmail.com>
2026-01-29 12:43:33 +0100

    Add Range class to represent numeric intervals

---
+++ NackEngine/core/Range.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NackEngine.core
+{
+    public class Range
+    {
+        private double min;
+        private double max;
+
+        // Default
+        public static readonly Range EMPTY = new Range(double.MaxValue, double.MinValue);
+        public static readonly Range UNIVERSE = new Range(double.MinValue, double.MaxValue);
+
+        public Range()
+        {
+            this.min = double.MinValue; this.max = double.MaxValue;
+        }
+
+        public Range(double min, double max) {
+            this.min = min; this.max=max;
+        }
+
+        public double Size() {
+            return max - min;
+        }
+
+        public bool Contains(double z) { 
+            return min <= z && z <= max;
+        }
+
+        public bool Surrounds(double z) {
+            return min < z && z < max;
+        }
+
+        public double Min() {
+            return min;
+        }
+
+        public double Max() {
+            return max;
+        }
+
+        public double Clamp(double x)
+        {
+            if (x < min) return min;
+            if (x > max) return max;
+            return x;
+        }
+    }
+}
