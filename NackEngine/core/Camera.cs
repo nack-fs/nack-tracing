403690e5487311f60c2be7300046899c802502e7
Ignacio Fernández Suárez <ignaciofsoviedo@gmail.com>
2026-01-29 19:14:17 +0100

    Refactored camera and render logic to Camera class
    Separated camera setup and rendering into a new Camera class in NackEngine.core, encapsulating ray generation and image export.
    
    This improves code organization and reusability.

---
+++ NackEngine/core/Camera.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using ExportConfig;
+using System.Text;
+
+namespace NackEngine.core
+{
+    using Point = NVector;
+
+    public class Camera
+    {
+        public double aspectRatio;
+        public int imageWidth;
+        // -----------------------
+        private int imageHeight;
+        private Point cameraOrigin;
+        private Point pixel00;
+        private NVector deltaH;
+        private NVector deltaW;
+
+        public Camera() { 
+            this.aspectRatio = 1.0;
+            this.imageWidth = 100;
+        }
+
+        public Camera(double aspectRatio, int imageWidth)
+        {
+            this.aspectRatio = aspectRatio;
+            this.imageWidth = imageWidth;
+        }
+
+        public void Render(Hittable world) {
+            Initialize();
+
+            StringBuilder imageData = new StringBuilder();
+
+            for (int y = 0; y < imageHeight; y++)
+            {
+                for (int x = 0; x < imageWidth; x++)
+                {
+                    var center = pixel00 + (x * deltaH) + (y * deltaW);
+                    var rayDirection = center - cameraOrigin;
+                    Ray r = new Ray(cameraOrigin, rayDirection);
+                    Color color = RayColor(r, world);
+                    imageData.AppendLine(color.ToString());
+                }
+            }
+            PNGExport export = new PNGExport(imageWidth, imageHeight, "rendernew");
+            export.ExportFile(imageData);
+        }
+
+        private void Initialize() {
+            this.imageHeight = Math.Max(1, (int)(imageWidth / aspectRatio));
+
+            // Camera
+            var focalLenght = 1.0;
+            var viewportHeight = 2.0;
+            var viewportWidth = viewportHeight * ((double)imageWidth / imageHeight);
+
+            this.cameraOrigin = new Point(0, 0, 0);
+
+            /*
+             * Vectors
+             * - Across the horizontal
+             * - Down the vertical
+             */
+            var viewportH = new NVector(viewportWidth, 0, 0);
+            var viewportW = new NVector(0, -viewportHeight, 0);
+
+
+            // Horizontal and Vertical, Delta Vectors
+            this.deltaH = viewportH / imageWidth;
+            this.deltaW = viewportW / imageHeight;
+
+            var viewportUpperLeft = cameraOrigin -
+                new NVector(0, 0, focalLenght) - viewportH / 2 - viewportW / 2;
+
+            this.pixel00 = viewportUpperLeft + 0.5 * (deltaH + deltaW);
+        }
+
+        private Color RayColor(Ray ray, Hittable world) {
+            HitStruct hit;
+            if (world.Hit(ray, Range.DEFAULT, out hit))
+            {
+                return new Color(0.5 * (hit.normal + new Color(1, 1, 1).Vector()));
+            }
+
+            NVector unitDirection = NVector.UnitVector(ray.Direction());
+            var a = 0.5 * (unitDirection.Y() + 1.0);
+            Color white = new Color(1.0, 1.0, 1.0);
+            Color lightBlue = new Color(0.5, 0.7, 1.0);
+            return new Color(white.Vector() * (1.0 - a) + lightBlue.Vector() * a);
+        }
+    }
+}
--- NackTracing/Program.cs
+++ NackTracing/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text;
 using System.IO;
-using ExportConfig;
 using NackEngine.core;
 using NackEngine.objects;
 using Range = NackEngine.core.Range;
@@ -14,11 +13,6 @@ namespace NackTracing
     {
         static void Main(string[] args)
         {
-            // Image settings
-            var aspectRatio = 16.0 / 9.0;
-            int imageWidth = 400;
-
-            int imageHeight = Math.Max(1,(int)(imageWidth / aspectRatio));
 
             // World
             HitCollection world = new HitCollection();
@@ -28,55 +22,11 @@ namespace NackTracing
 
 
             // Camera
-            var focalLenght = 1.0;
-            var viewportHeight = 2.0;
-            var viewportWidth = viewportHeight * ((double)imageWidth / imageHeight);
-            var cameraOrigin = new Point(0, 0, 0);
+            double aspectRatio = 16.0 / 9.0;
+            int imageWidth = 400;
 
-            /*
-             * Vectors
-             * - Across the horizontal
-             * - Down the vertical
-             */
-            var viewportH = new NVector(viewportWidth,0,0);
-            var viewportW = new NVector(0, -viewportHeight, 0);
-
-            // Horizontal and Vertical, Delta Vectors
-            var deltaH = viewportH / imageWidth;
-            var deltaW = viewportW / imageHeight;
-
-            var viewportUpperLeft = cameraOrigin -
-                new NVector(0, 0, focalLenght) - viewportH / 2 - viewportW / 2;
-
-            var pixel00 = viewportUpperLeft + 0.5 * (deltaH + deltaW);
-
-            // Render
-            StringBuilder imageData = new StringBuilder();
-
-            for (int y = 0; y < imageHeight; y++) {
-                for (int x = 0; x < imageWidth; x++) {
-                    var center = pixel00 + (x*deltaH) + (y*deltaW);
-                    var rayDirection = center - cameraOrigin;
-                    Ray r = new Ray(cameraOrigin, rayDirection);
-                    Color color = rayColor(r, world);
-                    imageData.AppendLine(color.ToString());
-                }
-            }
-            PNGExport export = new PNGExport(imageWidth, imageHeight, "rendernew");
-            export.ExportFile(imageData);
-        }
-        private static Color rayColor(Ray ray, Hittable world)
-        {
-            HitStruct hit;
-            if (world.Hit(ray, Range.DEFAULT, out hit)) {
-                return new Color(0.5 * (hit.normal + new Color(1, 1, 1).Vector()));
-            }
-
-            NVector unitDirection = NVector.UnitVector(ray.Direction());
-            var a = 0.5 * (unitDirection.Y() + 1.0);
-            Color white = new Color(1.0, 1.0, 1.0);
-            Color lightBlue = new Color(0.5, 0.7, 1.0);
-            return new Color(white.Vector() * (1.0 - a) + lightBlue.Vector() * a);
+            Camera camera = new Camera(aspectRatio, imageWidth);
+            camera.Render(world);
         }
     }
 }
