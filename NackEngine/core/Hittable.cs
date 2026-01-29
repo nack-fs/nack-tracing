0b548426229c9a2981baf3ba91fe7b0bdc4ef358
Ignacio Fernández Suárez <ignaciofsoviedo@gmail.com>
2026-01-29 12:02:46 +0100

    Changed Hit method to use 'out' parameter for HitStruct in Hittable interface and implementations. Added Collection class to manage multiple Hittable objects and return the closest hit. Updated Sphere to match new signature.

--- NackEngine/core/Hittable.cs
+++ NackEngine/core/Hittable.cs
@@ -6,6 +6,6 @@ namespace NackEngine.core
 {
     public interface Hittable
     {
-        public bool Hit(Ray ray, double rayTmin, double rayTmax, HitStruct hit);
+        public bool Hit(Ray ray, double rayTmin, double rayTmax,out HitStruct hit);
     }
 }
--- NackEngine/objects/Sphere.cs
+++ NackEngine/objects/Sphere.cs
@@ -18,12 +18,13 @@ namespace NackEngine.objects
             this.radius = Math.Max(0, radius);
         }
 
-        public bool Hit(Ray ray, double rayTmin, double rayTmax, HitStruct hit)
+        public bool Hit(Ray ray, double rayTmin, double rayTmax,out HitStruct hit)
         {
             NVector oc = center - ray.Origin();
             var a = ray.Direction().LengthSquared();
             var h = NVector.Dot(ray.Direction(), oc);
             var c = oc.LengthSquared() - radius * radius;
+            hit = default;
 
             var discriminant = h * h - a * c;
             if (discriminant < 0) {
