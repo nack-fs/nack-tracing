4201ac96aca0637b3f95256d81fe8d0d3e490600
Ignacio Fernández Suárez <ignaciofsoviedo@gmail.com>
2026-02-06 17:02:28 +0100

    Add dielectric material and refraction support
    Added Dielectric class to handle transparent materials with refraction. Implemented NVector.Refract for vector refraction calculations. Updated Program.cs to use Dielectric for the left sphere instead of Metal, simulating glass-like behavior.

---
+++ NackEngine/materials/Dielectric.cs
@@ -0,0 +1,29 @@
+﻿using NackEngine.core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NackEngine.materials
+{
+    public class Dielectric : Material
+    {
+        private double indexRefraction;
+
+        public Dielectric(double indexRefraction)
+        {
+            this.indexRefraction = indexRefraction;
+        }
+
+        public bool Bounce(Ray ray, HitStruct hit, out Color attenuation, out Ray bounced)
+        {
+            attenuation = new Color(1.0,1.0,1.0);
+            double ri = hit.FrontFace ? (1.0 / indexRefraction) : indexRefraction;
+
+            NVector unitDirection = NVector.UnitVector(ray.Direction());
+            NVector refracted = NVector.Refract(unitDirection, hit.Normal, ri);
+
+            bounced = new Ray(hit.Point, refracted);
+            return true;
+        }
+    }
+}
